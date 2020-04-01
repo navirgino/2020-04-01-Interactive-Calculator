a013d52736ce103fc259c01d86bc29a6ed2dfb8c
Nico Virgino <nicovirgino@gmail.com>
2020-04-01 13:47:10 -0500

    added some styling

--- main.js
+++ main.js
@@ -8,11 +8,29 @@ $(document).ready(function(){
     let cheeseCarbCounter = +$('.carb_num').val();
     let cheeseProteinCounter = +$('.protein_num').val();
 
-    $('#cheese').click(function() {
-        $(".calorie_num").val(cheeseCalorieCounter+=cheeseCalorieCounter);
-        $(".fat_num").val(cheeseFatCounter+=cheeseFatCounter);
-        $(".carb_num").val(cheeseCarbCounter+=cheeseCarbCounter);
-        $(".protein_num").val(cheeseProteinCounter+=cheeseProteinCounter);
+
+
+
+    $('#original_cheese_pizza').click(function() {
+
+        //bug on hover effect, doesn't apply css except on when you scroll down then scroll back up?????
+        //then gets stuck on hover effect
+        $('#original_cheese_pizza').hover(
+            function() {
+                $(this).css('opacity', '0.5');
+            },
+            function() {
+                $(this).css('opacity', '1.0');}
+        );
+
+        $('#pizza').append("<img class='cheese_pizza' src=\"img/cheese_pizza.png\">");
+
+        $('#total_fat').html(cheeseFatCounter+=13);
+        $('#total_carb').html(cheeseCarbCounter+=33);
+        $('#total_protein').html(cheeseProteinCounter+=13);
+        $('#total_calories').html(cheeseCalorieCounter+=310);
+
+
 
     });
 
